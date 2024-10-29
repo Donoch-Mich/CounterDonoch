f39ecb8ec4c8668f8efb71ac60f99f4f49dc2c0d
Donoch-Mich <donochmich3@gmail.com>
2024-10-29 03:22:13 -0700

    com

---
+++ Counter/Controls/element.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Maui.Controls;
+using System.Xml.Linq;
+
+namespace Counter.Controls
+{
+    public partial class element : ContentView
+    {
+        private int _counterValue;
+
+        public static readonly BindableProperty CounterNameProperty =
+            BindableProperty.Create(nameof(CounterName), typeof(string), typeof(element), default(string), propertyChanged: OnCounterNameChanged);
+
+        public static readonly BindableProperty InitialValueProperty =
+            BindableProperty.Create(nameof(InitialValue), typeof(int), typeof(element), 0, propertyChanged: OnInitialValueChanged);
+
+        public string CounterName
+        {
+            get => (string)GetValue(CounterNameProperty);
+            set => SetValue(CounterNameProperty, value);
+        }
+
+        public int InitialValue
+        {
+            get => (int)GetValue(InitialValueProperty);
+            set => SetValue(InitialValueProperty, value);
+        }
+
+        public element()
+        {
+            InitializeComponent();
+            UpdateCounterDisplay();
+        }
+
+        private static void OnCounterNameChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (element)bindable;
+            control.Counter_Name.Text = (string)newValue;
+        }
+
+        private static void OnInitialValueChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (element)bindable;
+            control._counterValue = (int)newValue;
+            control.UpdateCounterDisplay();
+        }
+
+        private void OnPlusCounterClicked(object sender, EventArgs e)
+        {
+            _counterValue++;
+            UpdateCounterDisplay();
+            SaveCounter(sender, e);
+        }
+
+        private void OnMinusCounterClicked(object sender, EventArgs e)
+        {
+            _counterValue--;
+            UpdateCounterDisplay();
+            SaveCounter(sender, e);
+        }
+
+        private void ResetCounter(object sender, EventArgs e)
+        {
+            _counterValue = InitialValue;
+            UpdateCounterDisplay();
+            SaveCounter(sender, e);
+        }
+
+        private void UpdateCounterDisplay()
+        {
+            Result.Text = _counterValue.ToString();
+            
+        }
+        private void SaveCounter(object sender, EventArgs e)
+        {
+            var filePath = Path.Combine(FileSystem.AppDataDirectory, "counterData.xml");
+            XDocument saveFile;
+
+            
+            if (File.Exists(filePath))
+            {
+                saveFile = XDocument.Load(filePath);
+            }
+            else
+            {
+                saveFile = new XDocument(new XElement("Counters"));
+            }
+
+            
+            var existingCounter = saveFile.Root.Elements("Counter")
+                .FirstOrDefault(x => (string)x.Element("CounterName") == CounterName);
+
+            if (existingCounter != null)
+            {
+                // Update the existing counter's values
+                existingCounter.Element("InitialValue").Value = InitialValue.ToString();
+                existingCounter.Element("CounterValue").Value = _counterValue.ToString();
+            }
+            else
+            {
+                var newCounter = new XElement("Counter",
+                    new XElement("CounterName", CounterName),
+                    new XElement("InitialValue", InitialValue),
+                    new XElement("CounterValue", _counterValue)
+                );
+
+                saveFile.Root.Add(newCounter);
+            }
+
+            saveFile.Save(filePath);
+        }
+        public void SetCounterValue(int value)
+        {
+            _counterValue = value;
+            UpdateCounterDisplay();
+        }
+
+
+
+
+    }
+}
--- Counter/MainPage.xaml.cs
+++ Counter/MainPage.xaml.cs
@@ -1,56 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using Microsoft.Maui.Controls;
 
 namespace Counter
 {
-    public partial class MainPage
+    public partial class MainPage : ContentPage
     {
-        int basicValue = 0;
-
-        int value = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            LoadCounters();
         }
 
-        private void OnPlusCounterClicked(object sender, EventArgs e)
+        private void addCounterName_Clicked(object sender, EventArgs e)
         {
-            value++;
+            string counterName = string.IsNullOrWhiteSpace(CounterNameEntry.Text) ? "Domyślna nazwa" : CounterNameEntry.Text;
 
-            ShowResult();
+            int initialValue = 0;
+            if (!string.IsNullOrEmpty(CounterValueEntry.Text) && int.TryParse(CounterValueEntry.Text, out int parsedValue))
+            {
+                initialValue = parsedValue;
+            }
+            else
+            {
+                DisplayAlert("Invalid Input", "Please enter a valid initial value.", "OK");
+                return;
+            }
+
+            var customElement = new Counter.Controls.element
+            {
+                CounterName = counterName,
+                InitialValue = initialValue
+            };
+
+            Controls.Children.Add(customElement);
+
+            CounterNameEntry.Text = string.Empty;
+            CounterValueEntry.Text = string.Empty;
         }
 
-        private void OnMinusCounterClicked(object sender, EventArgs e)
+        private void LoadCounters()
         {
-            value--;
+            var filePath = Path.Combine(FileSystem.AppDataDirectory, "counterData.xml");
 
-            ShowResult();
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    var doc = XDocument.Load(filePath);
+
+                    foreach (var counterElement in doc.Descendants("Counter"))
+                    {
+                        string counterName = counterElement.Element("CounterName")?.Value ?? "Default Name";
+                        int initialValue = int.TryParse(counterElement.Element("InitialValue")?.Value, out int parsedInitialValue) ? parsedInitialValue : 0;
+                        int counterValue = int.TryParse(counterElement.Element("CounterValue")?.Value, out int parsedCounterValue) ? parsedCounterValue : initialValue;
+
+                        var customElement = new Counter.Controls.element
+                        {
+                            CounterName = counterName,
+                            InitialValue = initialValue
+                        };
+
+                        // Set _counterValue directly using SetCounterValue
+                        customElement.SetCounterValue(counterValue);
+
+                        Controls.Children.Add(customElement);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Error", "Failed to load counters: " + ex.Message, "OK");
+                }
+            }
         }
-
-        private void ShowResult()
-        {
-
-            Result.Text =  $"{value}";
-                
-            SemanticScreenReader.Announce(Result.Text);
-        }
-
-        private void addCounter(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void ReserCounter(object sender, EventArgs e)
-        {
-            value = basicValue;
-            ShowResult();
-        }
-        
-
+    
     }
 }
-
