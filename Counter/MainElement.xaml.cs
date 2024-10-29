504d4631217367ccab89d20e1a28b44cd9be34f0
Donoch-Mich <donochmich3@gmail.com>
2024-10-29 00:50:00 -0700

    first commit

---
+++ Counter/MainElement.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public partial class MainElement
+    {
+        int count = 0;
+
+        public MainElement()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPlusCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            ShowResult();
+        }
+
+        private void OnMinusCounterClicked(object sender, EventArgs e)
+        {
+            count--;
+
+            ShowResult();
+        }
+
+        private void ShowResult()
+        {
+
+            Result.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(Result.Text);
+        }
+    }
+}
+
---
+++ Counter/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public partial class MainPage
+    {
+        int basicValue = 0;
+
+        int value = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPlusCounterClicked(object sender, EventArgs e)
+        {
+            value++;
+
+            ShowResult();
+        }
+
+        private void OnMinusCounterClicked(object sender, EventArgs e)
+        {
+            value--;
+
+            ShowResult();
+        }
+
+        private void ShowResult()
+        {
+
+            Result.Text =  $"{value}";
+                
+            SemanticScreenReader.Announce(Result.Text);
+        }
+
+        private void addCounter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ReserCounter(object sender, EventArgs e)
+        {
+            value = basicValue;
+            ShowResult();
+        }
+        
+
+    }
+}
+
